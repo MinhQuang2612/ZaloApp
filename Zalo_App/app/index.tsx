a5c612efe6decf43816897ecac79fc7c1af07557
MinhQuang2612 <minhquang.yi@gmail.com>
2025-03-05 21:45:05 +0700

    create project

---
+++ Zalo_App/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>You"re piece of shit, no one care if u go missing!</Text>
+    </View>
+  );
+}
